4e521b8f5ff0b6a24fae6f3b69a60b33cee6d179
ByeongHunKim <rkwhr912@naver.com>
2022-05-14 15:43:14 +0900

    #2 숫자 맞추기 코드 추가

--- 220514/main.rs
+++ 220514/main.rs
@@ -1,3 +1,22 @@
 fn main() {
   println!("Hello, word!");
 }
+
+// 
+
+use std::io;
+
+fn main(){
+  println!("숫자를 추측해보겠습니다");
+  println!("숫자를 입력해보세요");
+  let mut guess = String::new();
+  io::stdin().read_line(&mut guess)
+      .expect("Failed to read line");
+  println!("당신의 숫자는: {} 입니다.", guess);
+}
+
+// 
+
+fn main() {
+  println!("Hello, world!");
+}
